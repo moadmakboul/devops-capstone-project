7da54c73cf5943182a071dc7d364d9fa5445d278
moadmakboul <moad.makboul.mm@gmail.com>
2023-02-05 08:47:56 -0500

    CI workflow setup

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -181,4 +181,8 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertIsNone(data)
 
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        response = self.client.delete(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
